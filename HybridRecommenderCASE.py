b38f559dde31b322eb7ce1dc625d0d86ab25cf2a
Ayça Nur İşçioğlu <aycanurisciogluu@gmail.com>
2022-11-22 11:42:46 +0300

    Update HybridRecommenderCASE.py

--- HybridRecommenderCASE.py
+++ HybridRecommenderCASE.py
@@ -1,12 +1,3 @@
-import pandas as pd
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 500)
-pd.set_option('display.expand_frame_repr', False)
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
 movie = pd.read_csv('datasets/movie.csv')
 rating = pd.read_csv('datasets/rating.csv')
 
